7fd06c3b01064ebfa98b2c9cfba786f5e5e8c9c2
juseojo <j86371224@gmail.com>
2025-05-07 14:12:35 +0900

    init

---
+++ Random_color/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random_color
+//
+//  Created by seongjun cho on 5/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
