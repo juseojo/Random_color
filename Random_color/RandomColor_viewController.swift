bcdb01bd98cc405bb33068aaae4ca09bb3b163fc
juseojo <j86371224@gmail.com>
2025-05-07 15:51:49 +0900

    [Feat] UI
    - creat UI of RandomColor

--- Random_color/RandomColor_viewController.swift
+++ Random_color/RandomColor_viewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class RandomColor_viewController: UIViewController {
 
+	private let randomColorView = RandomColor_view()
+
+	override func loadView() {
+		   view = randomColorView
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
-
 }
 
